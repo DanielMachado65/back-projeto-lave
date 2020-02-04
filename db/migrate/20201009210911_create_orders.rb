46abdf21a2bf80df96ee98e9711dde12e32bd1e5
Daniel Machado <danielmachadopintos@gmail.com>
2020-02-04 01:03:00 -0300

    feat(order): generete scaffold order
    Geração do Order

---
+++ db/migrate/20201009210911_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.string :dealine
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
